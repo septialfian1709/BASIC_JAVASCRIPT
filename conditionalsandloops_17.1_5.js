d857fe4e52a18dac11e613dc5e7eb593b6c7decd
Septiani <septialfian@gmail.com>
2022-10-04 16:36:09 +0700

    04102022_pwpb_basicjava_conditionalorboolean

---
+++ conditionalsandloops_17.1_5.js
@@ -0,0 +1,11 @@
+var x = 3;
+switch (x) {
+    case 1:
+        console.log(x)
+        break;
+    case 2:
+        console.log(x+2);
+        break;
+    default:
+        console.log(x+5);
+}
---
+++ conditionalsandloops_17.1_6.js
@@ -0,0 +1,15 @@
+var color ="yellow";
+switch(color) {
+    case "blue":
+        console.log("This is blue");
+        break;
+    case "red":
+        console.log("This is Red");
+        break;
+    case "green":
+        console.log("This is green");
+    case "Orange":
+        console.log("This is orange");
+    default:
+        console.log("Color not found");
+}
